cb80721258fc239585c0e9bc94ba90308ea0ddfe
Elisabeth Wenger-Stickel <elsie19@pm.me>
2026-04-25 18:36:28 -0400

    testing

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,8 +1,20 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.diamond = {
+    install_info = {
+        url = "https://github.com/Elsie19/diamond-grammar",
+        files = { "src/parser.c" },
+        generate_reqires_npm = false,
+        requires_generate_from_grammar = false,
+    },
+    -- The filetype you want it registered as
+    filetype = "di",
+}
+
 vim.filetype.add({ extension = { diamond = "di" } })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "di",
-	callback = function(event)
-		vim.bo[event.buf].commentstring = "# %s"
-	end,
+    pattern = "di",
+    callback = function(event)
+        vim.bo[event.buf].commentstring = "# %s"
+    end,
 })
